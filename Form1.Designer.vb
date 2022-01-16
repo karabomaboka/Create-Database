c2f40e6410b7811fcef0d4abf9ee0e22177182af
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2022-01-16 10:02:42 +0200

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,202 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+  Inherits System.Windows.Forms.Form
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+    Try
+      If disposing AndAlso components IsNot Nothing Then
+        components.Dispose()
+      End If
+    Finally
+      MyBase.Dispose(disposing)
+    End Try
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+    Me.Label1 = New System.Windows.Forms.Label()
+    Me.Label2 = New System.Windows.Forms.Label()
+    Me.Label3 = New System.Windows.Forms.Label()
+    Me.Label4 = New System.Windows.Forms.Label()
+    Me.Label5 = New System.Windows.Forms.Label()
+    Me.TextBox1 = New System.Windows.Forms.TextBox()
+    Me.TextBox2 = New System.Windows.Forms.TextBox()
+    Me.TextBox3 = New System.Windows.Forms.TextBox()
+    Me.TextBox4 = New System.Windows.Forms.TextBox()
+    Me.TextBox5 = New System.Windows.Forms.TextBox()
+    Me.TextBox6 = New System.Windows.Forms.TextBox()
+    Me.Label6 = New System.Windows.Forms.Label()
+    Me.Button1 = New System.Windows.Forms.Button()
+    Me.Button2 = New System.Windows.Forms.Button()
+    Me.SuspendLayout()
+    '
+    'Label1
+    '
+    Me.Label1.AutoSize = True
+    Me.Label1.Location = New System.Drawing.Point(31, 15)
+    Me.Label1.Name = "Label1"
+    Me.Label1.Size = New System.Drawing.Size(49, 17)
+    Me.Label1.TabIndex = 0
+    Me.Label1.Text = "Name:"
+    '
+    'Label2
+    '
+    Me.Label2.AutoSize = True
+    Me.Label2.Location = New System.Drawing.Point(16, 43)
+    Me.Label2.Name = "Label2"
+    Me.Label2.Size = New System.Drawing.Size(64, 17)
+    Me.Label2.TabIndex = 1
+    Me.Label2.Text = "Address:"
+    '
+    'Label3
+    '
+    Me.Label3.AutoSize = True
+    Me.Label3.Location = New System.Drawing.Point(45, 71)
+    Me.Label3.Name = "Label3"
+    Me.Label3.Size = New System.Drawing.Size(35, 17)
+    Me.Label3.TabIndex = 2
+    Me.Label3.Text = "City:"
+    '
+    'Label4
+    '
+    Me.Label4.AutoSize = True
+    Me.Label4.Location = New System.Drawing.Point(35, 99)
+    Me.Label4.Name = "Label4"
+    Me.Label4.Size = New System.Drawing.Size(45, 17)
+    Me.Label4.TabIndex = 3
+    Me.Label4.Text = "State:"
+    '
+    'Label5
+    '
+    Me.Label5.AutoSize = True
+    Me.Label5.Location = New System.Drawing.Point(27, 127)
+    Me.Label5.Name = "Label5"
+    Me.Label5.Size = New System.Drawing.Size(53, 17)
+    Me.Label5.TabIndex = 4
+    Me.Label5.Text = "Phone:"
+    '
+    'TextBox1
+    '
+    Me.TextBox1.Location = New System.Drawing.Point(84, 12)
+    Me.TextBox1.Name = "TextBox1"
+    Me.TextBox1.Size = New System.Drawing.Size(263, 22)
+    Me.TextBox1.TabIndex = 5
+    Me.TextBox1.Text = "Mr. President"
+    '
+    'TextBox2
+    '
+    Me.TextBox2.Location = New System.Drawing.Point(84, 40)
+    Me.TextBox2.Name = "TextBox2"
+    Me.TextBox2.Size = New System.Drawing.Size(263, 22)
+    Me.TextBox2.TabIndex = 6
+    Me.TextBox2.Text = "1600 Pennsylvania Avenue"
+    '
+    'TextBox3
+    '
+    Me.TextBox3.Location = New System.Drawing.Point(84, 68)
+    Me.TextBox3.Name = "TextBox3"
+    Me.TextBox3.Size = New System.Drawing.Size(263, 22)
+    Me.TextBox3.TabIndex = 7
+    Me.TextBox3.Text = "Washington"
+    '
+    'TextBox4
+    '
+    Me.TextBox4.Location = New System.Drawing.Point(84, 96)
+    Me.TextBox4.Name = "TextBox4"
+    Me.TextBox4.Size = New System.Drawing.Size(100, 22)
+    Me.TextBox4.TabIndex = 8
+    Me.TextBox4.Text = "DC"
+    '
+    'TextBox5
+    '
+    Me.TextBox5.Location = New System.Drawing.Point(84, 124)
+    Me.TextBox5.Name = "TextBox5"
+    Me.TextBox5.Size = New System.Drawing.Size(100, 22)
+    Me.TextBox5.TabIndex = 9
+    Me.TextBox5.Text = "202-456-1414"
+    '
+    'TextBox6
+    '
+    Me.TextBox6.Location = New System.Drawing.Point(285, 96)
+    Me.TextBox6.Name = "TextBox6"
+    Me.TextBox6.Size = New System.Drawing.Size(62, 22)
+    Me.TextBox6.TabIndex = 10
+    Me.TextBox6.Text = "20500"
+    '
+    'Label6
+    '
+    Me.Label6.AutoSize = True
+    Me.Label6.Location = New System.Drawing.Point(212, 99)
+    Me.Label6.Name = "Label6"
+    Me.Label6.Size = New System.Drawing.Size(67, 17)
+    Me.Label6.TabIndex = 11
+    Me.Label6.Text = "Zip code:"
+    '
+    'Button1
+    '
+    Me.Button1.Location = New System.Drawing.Point(38, 152)
+    Me.Button1.Name = "Button1"
+    Me.Button1.Size = New System.Drawing.Size(146, 36)
+    Me.Button1.TabIndex = 12
+    Me.Button1.Text = "Write to Database"
+    Me.Button1.UseVisualStyleBackColor = True
+    '
+    'Button2
+    '
+    Me.Button2.Location = New System.Drawing.Point(235, 152)
+    Me.Button2.Name = "Button2"
+    Me.Button2.Size = New System.Drawing.Size(85, 36)
+    Me.Button2.TabIndex = 13
+    Me.Button2.Text = "Exit"
+    Me.Button2.UseVisualStyleBackColor = True
+    '
+    'Form1
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(374, 200)
+    Me.Controls.Add(Me.Button2)
+    Me.Controls.Add(Me.Button1)
+    Me.Controls.Add(Me.Label6)
+    Me.Controls.Add(Me.TextBox6)
+    Me.Controls.Add(Me.TextBox5)
+    Me.Controls.Add(Me.TextBox4)
+    Me.Controls.Add(Me.TextBox3)
+    Me.Controls.Add(Me.TextBox2)
+    Me.Controls.Add(Me.TextBox1)
+    Me.Controls.Add(Me.Label5)
+    Me.Controls.Add(Me.Label4)
+    Me.Controls.Add(Me.Label3)
+    Me.Controls.Add(Me.Label2)
+    Me.Controls.Add(Me.Label1)
+    Me.Name = "Form1"
+    Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+    Me.Text = "Create Database"
+    Me.ResumeLayout(False)
+    Me.PerformLayout()
+
+  End Sub
+
+  Friend WithEvents Label1 As Label
+  Friend WithEvents Label2 As Label
+  Friend WithEvents Label3 As Label
+  Friend WithEvents Label4 As Label
+  Friend WithEvents Label5 As Label
+  Friend WithEvents TextBox1 As TextBox
+  Friend WithEvents TextBox2 As TextBox
+  Friend WithEvents TextBox3 As TextBox
+  Friend WithEvents TextBox4 As TextBox
+  Friend WithEvents TextBox5 As TextBox
+  Friend WithEvents TextBox6 As TextBox
+  Friend WithEvents Label6 As Label
+  Friend WithEvents Button1 As Button
+  Friend WithEvents Button2 As Button
+End Class
